2a71536ccada5f7e30ea55dc3d794b104ddb3960
Marx <domah88@gmail.com>
2017-06-03 12:04:47 -0500

    Added border radius and box-shadow to fido

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.body.style.backgroundColor = "red";
 document.getElementById("aboutMe").style.backgroundColor = "blue";
 document.body.style.fontFamily = "sans-serif";
-document.getElementById("nickname").innerHTML = "Marx";
-document.getElementById("favorites").innerHTML = "Red Wine";
-document.getElementById("hometown").innerHTML = "Minneapolis";
+document.getElementById("nickname").innerHTML = "Fido";
+document.getElementById("favorites").innerHTML = "Milk";
+document.getElementById("hometown").innerHTML = "London";
 
 var x = document.getElementsByClassName("listitem");
 
@@ -13,5 +13,7 @@ for (var i = 0; i < x.length; i++){
 
 var fido = document.createElement("img");
 fido.src = "http://findingfido.ca/wp-content/themes/FindingFido/images/menu_headers/considering/considering-off_en.jpg";
+fido.style.borderRadius = '10px';
+fido.style.boxShadow = "20px 10px 10px #000";
 document.body.appendChild(fido);
 

8294ec4470f340563ec55f10d4bcf3248234d946
Marx <domah88@gmail.com>
2017-06-03 10:30:31 -0500

    Submitting homework #4

---
+++ script.js
@@ -0,0 +1,10 @@
+document.body.style.backgroundColor = "red";
+document.getElementById("aboutMe").style.backgroundColor = "blue";
+document.body.style.fontFamily = "sans-serif";
+document.getElementById("nickname").innerHTML = "Marx";
+document.getElementById("favorites").innerHTML = "Red Wine";
+document.getElementById("hometown").innerHTML = "Minneapolis";
+
+var i = 
+
+
